6255e4a4116f81615cadf9e8017d20ed2ed666e8
WGCAT <seed9878@naver.com>
2024-05-29 04:26:04 +0900

    (feat): 사건 조회 repository 추가

--- database/repository/location/location.go
+++ database/repository/location/location.go
@@ -7,7 +7,8 @@ import (
 )
 
 type LocationRepository interface {
-	GetByID(id uint) (*entity.Map, error)
+	FindEventByLocation(Id uint) (*entity.Map, error)
+	FindDexById(Id uint) (*entity.Map, error)
 }
 
 type gormLocationRepository struct {
@@ -18,6 +19,26 @@ func NewLocationRepository(db *gorm.DB) LocationRepository {
 	return &gormLocationRepository{db}
 }
 
-func (g *gormLocationRepository) GetByID(id uint) (*entity.Map, error) {
+// 사건 조회
+func (g *gormLocationRepository) FindEventByLocation(Id uint) (res *entity.Map, err error) {
+	// 1. 쿼리작성
+	// 	select id, latitude , longitude
+	//   from "map"
+	//  where id = 1;
+
+	// 2. gorm 적용
+	tx := g.db
+	err = tx.Model(&entity.Map{}).Select("id", "latitude", "longitude").Where("id = ?", Id).First(&res).Error
+
+	if err != nil {
+		return
+	}
+
+	return
+
+}
+
+// 사건 조회 : isCollect 사건 보유 여부
+func (g *gormLocationRepository) FindDexById(id uint) (*entity.Map, error) {
 	panic("")
 }
