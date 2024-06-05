ffb03df579808656bf5a88fe5c14020e22e6ede0
WGCAT <seed9878@naver.com>
2024-06-05 18:59:28 +0900

    (feat): 토큰검증 middleware 추가

--- app/api/location/location.go
+++ app/api/location/location.go
@@ -11,6 +11,5 @@ import (
 func SetLocationApis(route fiber.Router) {
 	h := handler.NewLocationHandler()
 	// 사건 조회
-	route.Get(constant.LocationPath().Location.FindEvent, middleware.JWTMiddleware, h.FindEvent)
-
+	route.Get(constant.LocationPath().Location.FindEvent, middleware.AuthVerificationMiddleware, h.FindEvent)
 }
