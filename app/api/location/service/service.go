0b2f9f7da16df51c194dbb6830b0cc2f5e07d8e5
WGCAT <seed9878@naver.com>
2024-06-14 13:52:45 +0900

    (feat): 사건 조회 service 추가

--- app/api/location/service/service.go
+++ app/api/location/service/service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"main/app/api/common"
 	"main/app/api/location/resource"
 	"main/database/repository"
 )
@@ -22,25 +23,20 @@ type locationService struct {
 func (s *locationService) FindEvent(userId uint, req *resource.FindEventRequest) (res *resource.FindEventResponse, err error) {
 	locationRepository := repository.NewRepository()
 
-	// 1. isCollect 참거짓 구분
-	countDex, err := locationRepository.FindDexById(userId)
-	// 2. 만약 값이 0이 아니라면 에러 반환
-	if countDex != 0 {
-		return nil, err
-		// 3. 만약 값이 0이면 만들어놓은 레포지토리를 사용해 데이터를 가져온다
-	} else {
-		eventFind, err := locationRepository.FindEventByLocation(req.Latitude, req.Longitude)
-		if err != nil {
-			return nil, err
-		}
-		// 4. 가져온 데이터를 res에 넣는다
-		res = &resource.FindEventResponse{
-			Id:        eventFind.ID,
-			Latitude:  eventFind.Latitude,
-			Longitude: eventFind.Longitude,
-			// IsCollect: ,
-		}
+	MaxLatLon, MinLatLon := common.CalculateLatLonRange(req.Latitude, req.Longitude, common.RadiusKm)
+
+	point, err := locationRepository.FindEventByPoint(MaxLatLon, MinLatLon)
+
+	userlist, err := common.GetIsCollectGrpc(userId)
+
+	// 4. 가져온 데이터를 res에 넣는다
+	res = &resource.FindEventResponse{
+		EventId:   point.EventID,
+		Latitude:  point.Latitude,
+		Longitude: point.Longitude,
+		IsCollect: userlist,
 	}
+
 	return
 
 }
