7bd50de36468a6eb50a468af8a49a90e2fc3edc5
WGCAT <seed9878@naver.com>
2024-06-16 01:32:58 +0900

    (docs): 주석 수정

--- app/api/location/handler/handler.go
+++ app/api/location/handler/handler.go
@@ -26,8 +26,8 @@ func NewLocationHandler() handler {
 // 사건 조회 query : latitude longitude , header : userId
 func (h *locationHandler) FindEvent(c *fiber.Ctx) error {
 	ctx := fiberkit.FiberKit{C: c}
-	req := new(resource.FindEventRequest)
 
+	req := new(resource.FindEventRequest)
 	ctx.C.BodyParser(req)
 
 	// 1. 쿼리파라미터에서 위도경도 받기
@@ -39,13 +39,10 @@ func (h *locationHandler) FindEvent(c *fiber.Ctx) error {
 	// 2. middleware에서 user_id 받기
 	req.UserId = uint(ctx.GetLocalsUint(common.LOCALS_USER_ID))
 
-	// req.UserId = uint(8)
 	// 3. 서비스 함수 실행
 	res, err := h.service.FindEvent(req.UserId, req)
 	if err != nil {
 		return ctx.HttpFail(err.Error(), fiber.StatusNotFound)
 	}
-
-	// 4. 리턴
 	return ctx.HttpOK(res)
 }
--- database/repository/location/location.go
+++ database/repository/location/location.go
@@ -21,12 +21,11 @@ func NewLocationRepository(db *gorm.DB) LocationRepository {
 
 // 사건 조회
 func (g *gormLocationRepository) FindEventByPoint(MaxLatLon, MinLatLon common.Point) (res *entity.Map, err error) {
-	// 1. 쿼리작성
-	// 	select id, latitude , longitude
+	// 	select event_id, latitude , longitude
 	//   from "map"
 	//  where  latitude BETWEEN 최소위도 AND 최대위도 AND longitude  BETWEEN 최소경도 AND 최대경도
 
-	// 2. gorm 적용
+	// 1. gorm 적용
 	err = g.db.Model(&entity.Map{}).Select("event_id", "latitude", "longitude").
 		Where("latitude BETWEEN ? AND ? AND longitude BETWEEN ? AND ?", MinLatLon.Latitude, MaxLatLon.Latitude, MinLatLon.Longitude, MaxLatLon.Longitude).
 		First(&res).Error
