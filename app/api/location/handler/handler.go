5ed06a32b2589e054e9bc33da396fdab4c33eae7
WGCAT <seed9878@naver.com>
2024-08-30 01:59:38 +0900

    (feat): 위치 리스트 조회 핸들러 추가

--- app/api/location/handler/handler.go
+++ app/api/location/handler/handler.go
@@ -11,6 +11,7 @@ import (
 
 type handler interface {
 	FindEvent(c *fiber.Ctx) error
+	FindLocationList(c *fiber.Ctx) error
 }
 
 type locationHandler struct {
@@ -36,11 +37,35 @@ func (h *locationHandler) FindEvent(c *fiber.Ctx) error {
 		return ctx.HttpFail(err.Error(), fiber.StatusNotFound)
 	}
 
-	// 2. middleware에서 user_id 받기
+	// 2. middleware에서 userid 받기
 	req.UserId = uint(ctx.GetLocalsUint(common.LOCALS_USER_ID))
 
 	// 3. 서비스 함수 실행
 	res, err := h.service.FindEvent(req.UserId, req)
+	if err != nil {
+		return ctx.HttpFail(err.Error(), fiber.StatusNotFound)
+	}
+	return ctx.HttpOK(res)
+}
+
+// 위치 리스트 조회 query : latitude longitude , header : userId
+func (h *locationHandler) FindLocationList(c *fiber.Ctx) error {
+	ctx := fiberkit.FiberKit{C: c}
+
+	req := new(resource.FindLocationListRequest)
+	ctx.C.BodyParser(req)
+
+	// 1. 쿼리파라미터에서 위도경도 받기
+	err := ctx.C.QueryParser(req)
+	if err != nil {
+		return ctx.HttpFail(err.Error(), fiber.StatusNotFound)
+	}
+
+	// 2. middleware에서 userid 받기
+	req.UserId = uint(ctx.GetLocalsUint(common.LOCALS_USER_ID))
+
+	// 3. 서비스 함수 실행
+	res, err := h.service.FindLocationList(req.UserId, req)
 	if err != nil {
 		return ctx.HttpFail(err.Error(), fiber.StatusNotFound)
 	}
