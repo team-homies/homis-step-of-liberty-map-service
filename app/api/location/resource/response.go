34895c363a1eaba0ac554b11ea91b18bf6f079d3
WGCAT <seed9878@naver.com>
2024-08-30 01:57:33 +0900

    (feat): 위치 리스트 조회 리소스 추가

--- app/api/location/resource/response.go
+++ app/api/location/resource/response.go
@@ -1,8 +1,26 @@
 package resource
 
+type MapInfoResponse struct {
+	EventId     uint    `json:"event_id"`
+	Name        string  `json:"name"`
+	Address     string  `json:"address"`
+	AddressRoad string  `json:"address_road"`
+	Latitude    float64 `json:"latitude"`
+	Longitude   float64 `json:"longitude"`
+}
 type FindEventResponse struct {
 	EventId   uint    `json:"event_id"`
 	Latitude  float64 `json:"latitude"`
 	Longitude float64 `json:"longitude"`
 	IsCollect bool    `json:"is_collect"`
+}
+
+type FindLocationListResponse struct {
+	EventId     uint    `json:"event_id"`
+	Latitude    float64 `json:"latitude"`
+	Longitude   float64 `json:"longitude"`
+	Distance    uint    `json:"distance"`
+	Address     string  `json:"address"`
+	AddressRoad string  `json:"address_road"`
+	IsCollect   bool    `json:"is_collect"`
 }
