46605045d059a75fd10a04c04c2523d6059be264
WGCAT <seed9878@naver.com>
2024-08-30 04:28:09 +0900

    (chore): 위치 리스트 리소스 자료형 변경

--- app/api/location/resource/response.go
+++ app/api/location/resource/response.go
@@ -19,7 +19,7 @@ type FindLocationListResponse struct {
 	EventId     uint    `json:"event_id"`
 	Latitude    float64 `json:"latitude"`
 	Longitude   float64 `json:"longitude"`
-	Distance    uint    `json:"distance"`
+	Distance    string  `json:"distance"`
 	Address     string  `json:"address"`
 	AddressRoad string  `json:"address_road"`
 	IsCollect   bool    `json:"is_collect"`
